72b5e261e789de9d9614a68d2b428342c28b0e26
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-04-27 12:11:14 -0400

    feature spec-user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,8 @@ class ApplicationController < ActionController::Base
 
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
