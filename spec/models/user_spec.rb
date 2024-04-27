4e38ddb90520f6d8f903a551ba73f49d93c71392
leandrodeleon22 <leandrodeleon22@gmail.com>
2024-04-27 12:12:57 -0400

    feature spec-user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe User, type: :model do
     it "is valid" do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test2@test.com',
         password: 'password',
         password_confirmation: 'password'
@@ -19,7 +18,7 @@ RSpec.describe User, type: :model do
       expect(user).to be_invalid
       expect(user.errors[:email]).to include("can't be blank")
   
-      user.email = 'test@test.com' # valid state
+      user.email = 'test@test.com' 
       user.valid?
       expect(user.errors[:email]).not_to include("can't be blank")
     end
@@ -29,25 +28,16 @@ RSpec.describe User, type: :model do
       expect(user).to be_invalid
       expect(user.errors[:name]).to include("can't be blank")
   
-      user.name = 'first_name' # valid state
+      user.name = 'first_name'
       user.valid? 
       expect(user.errors[:name]).not_to include("can't be blank")
     end
 
-    # it "last name is missing" do
-    #   user = User.new(last_name: nil)
-    #   expect(user).to be_invalid
-    #   expect(user.errors[:last_name]).to include("can't be blank")
-  
-    #   user.last_name = 'last_name' # valid state
-    #   user.valid? 
-    #   expect(user.errors[:last_name]).not_to include("can't be blank")
-    # end
+ 
 
     it "password don't match" do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test2@test.com',
         password: 'password',
         password_confirmation: 'pass'
@@ -59,7 +49,6 @@ RSpec.describe User, type: :model do
     it 'email must be unique' do
       user = User.new
       user.name = 'first_name'
-      # user.last_name = 'last_name'
       user.email = 'test@test.com'
       user.password = 'password'
       user.password_confirmation = 'password'
@@ -68,7 +57,6 @@ RSpec.describe User, type: :model do
     
       u = User.new
       u.name = 'first_name'
-      # u.last_name = 'last_name'
       u.email = 'test@test.com'
       u.password = 'password'
       u.password_confirmation = 'password'
@@ -80,7 +68,6 @@ RSpec.describe User, type: :model do
     it 'password length less than 5 characters is invalid' do
       user = User.new
       user.name = 'first_name'
-      # user.last_name = 'last_name'
       user.email = 'test@test.com'
       user.password = '1234'
       user.password_confirmation = '1234'
@@ -90,7 +77,6 @@ RSpec.describe User, type: :model do
     it 'password length must be at-least 5 characters' do
       user = User.new
       user.name = 'first_name'
-      # user.last_name = 'last_name'
       user.email = 'test@test.com'
       user.password = '12345'
       user.password_confirmation = '12345'
@@ -102,7 +88,6 @@ RSpec.describe User, type: :model do
     it 'should pass with valid credentials' do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test@test.com',
         password: 'password',
         password_confirmation: 'password'
@@ -116,7 +101,6 @@ RSpec.describe User, type: :model do
     it 'should not pass with invalid credentials' do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test@test.com',
         password: 'password',
         password_confirmation: 'password'
@@ -130,7 +114,6 @@ RSpec.describe User, type: :model do
     it 'should pass even with spaces present in email' do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test@test.com',
         password: 'password',
         password_confirmation: 'password'
@@ -144,7 +127,6 @@ RSpec.describe User, type: :model do
     it 'should pass even with caps present in email' do
       user = User.new(
         name: 'first_name',
-        # last_name: 'last_name',
         email: 'test@test.com',
         password: 'password',
         password_confirmation: 'password'
